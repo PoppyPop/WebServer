41a74063f5cfd83bb2b24a1cfeb649fb99e00755
PoppyPop <skytep@gmail.com>
2017-04-28 15:18:29 -0400

    VirtualHost script

--- virtualhost/virtualhost.sh
+++ virtualhost/virtualhost.sh
@@ -1,5 +1,92 @@
 #!/bin/bash
 
+### functions
+
+function setVirtualHostConfFile {
+	### create virtual host rules file
+	if ! echo "
+	<Directory $rootDir>
+		AllowOverride None
+		Require all denied
+	</Directory>
+		
+	<VirtualHost *:80>
+		ServerAdmin $email
+		ServerName $domain
+		ServerAlias *.$domain
+		DocumentRoot $rootDir/web
+				
+		# Possible values include: debug, info, notice, warn, error, crit,
+		# alert, emerg.
+		LogLevel warn
+
+		CustomLog $rootDir/logs/access.log combined
+		ErrorLog $rootDir/logs/error.log
+		
+		<Directory $rootDir/web>
+			Options +FollowSymLinks
+			AllowOverride all
+			Require all granted
+		</Directory>
+		
+		# suexec enabled
+		<IfModule mod_suexec.c>
+			SuexecUserGroup $owner $owner
+		</IfModule>
+		
+		# add support for apache mpm_itk
+		<IfModule mpm_itk_module>
+			AssignUserId $owner $owner
+		</IfModule>
+
+	</VirtualHost>
+	<VirtualHost *:443>
+		ServerAdmin $email
+		ServerName $domain
+		ServerAlias *.$domain
+		DocumentRoot $rootDir/web
+				
+		# Possible values include: debug, info, notice, warn, error, crit,
+		# alert, emerg.
+		LogLevel warn
+
+		CustomLog $rootDir/logs/access.log combined
+		ErrorLog $rootDir/logs/error.log
+		
+		<Directory $rootDir/web>
+			Options +FollowSymLinks
+			AllowOverride all
+			Require all granted
+		</Directory>
+		
+		# suexec enabled
+		<IfModule mod_suexec.c>
+			SuexecUserGroup $owner $owner
+		</IfModule>
+		
+		# add support for apache mpm_itk
+		<IfModule mpm_itk_module>
+			AssignUserId $owner $owner
+		</IfModule>
+		
+		<IfModule mod_ssl.c>
+			#SSLEngine on
+			#SSLProtocol All -SSLv2 -SSLv3
+			#SSLCertificateFile $rootDir/ssl/$domain.crt
+			#SSLCertificateKeyFile $rootDir/ssl/$domain.key
+		</IfModule>
+
+	</VirtualHost>" > $1
+	then
+		echo -e $"There is an ERROR creating $domain file"
+		exit;exit 4;
+	else
+		echo -e $"New Virtual Host Created"
+	fi
+}
+
+
+
 ### Set default parameters
 action=$1
 domain=$2
@@ -15,20 +102,13 @@ if [ "$(whoami)" != 'root' ]; then
 		exit 1;
 fi
 
-if [ "$action" != 'create' ] && [ "$action" != 'disable' ] && [ "$action" != 'delete' ]
-	then
-		echo $"You need to prompt for action (create or delete) -- Lower-case only"
-		exit 1;
-fi
-
 while [ "$domain" == "" ]
 do
 	echo -e $"Please provide domain. e.g.example.com"
 	read domain
 done
 
-if [ "$priority" == "" ] 
-	then
+if [ "$priority" == "" ]; then
 		priority=99
 fi
 
@@ -41,11 +121,7 @@ rootDir=$userDir$domain
 email="webmaster@$domain"
 owner=$(echo "$domain" | sed 's~[^[:alnum:]/]\+~~g')
 
-userindex=0
-while id "$owner" >/dev/null 2>&1; do
-	owner=$owner$userindex
-	userindex=$(( userindex + 1 ))
-done
+
 
 echo "====== Configuration ======"
 echo "Action    : $action"
@@ -54,27 +130,31 @@ echo "Priority  : $priority"
 echo "Conf File : $sitesAvailabledomain"
 echo "Root Dir  : $rootDir"
 echo "Email     : $email"
-echo "Sys User  : $owner"
 
-while true; do
-    read -p "Do you wish to commit this configuration?" yn
-    case $yn in
-        [Yy]* ) break;break;;
-        [Nn]* ) exit;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
-
-echo "Yes"
-exit;
 
 if [ "$action" == 'create' ]
 	then
 		### check if domain already exists
 		if [ -e $sitesAvailabledomain ]; then
 			echo -e $"This domain already exists.\nPlease Try Another one"
-			exit;
+			exit 2;
 		fi
+		
+		userindex=0
+		while id "$owner" >/dev/null 2>&1; do
+			owner=$owner$userindex
+			userindex=$(( userindex + 1 ))
+		done
+		echo "Sys User  : $owner"
+		
+		while true; do
+		    read -p "Do you wish to commit this configuration?" yn
+    		case $yn in
+        		[Yy]* ) break;break;;
+        		[Nn]* ) exit;;
+        		* ) echo "Please answer yes or no.";;
+    		esac
+		done
 		
 		### Create User
 		adduser --system --group --home $rootDir $owner
@@ -83,127 +163,67 @@ if [ "$action" == 'create' ]
 		if ! [ -d $rootDir ]; then
 			### create the directory
 			mkdir $rootDir
-			
-			### create subdirectory (data and logs
-			mkdir $rootDir/web
-			mkdir $rootDir/logs
-			mkdir $rootDir/ssl
-			
-			### give permission to root dir (A+R)
-			chmod 700 $rootDir
-			
-			### write test file in the new domain dir
-			if ! echo "<?php echo phpinfo(); ?>" > $rootDir/web/phpinfo.php
-			then
-				echo $"ERROR: Not able to write in file $rootDir/phpinfo.php. Please check permissions"
-				exit;
-			else
-				echo $"Added content to $rootDir/phpinfo.php"
-			fi
-			
-			### give permission to subdirectory (A+RX)
-			chmod 740 $rootDir/web
-			chmod 740 $rootDir/logs
-			chmod 740 $rootDir/ssl
-			
-			### change user
-			chown -R $owner:$owner $rootDir
-		fi
-
-		### create virtual host rules file
-		if ! echo "
-		<Directory $rootDir>
-			AllowOverride None
-			Order Deny,Allow
-			Deny from all
-		</Directory>
-			
-		<VirtualHost *:80>
-			ServerAdmin $email
-			ServerName $domain
-			ServerAlias *.$domain
-			DocumentRoot $rootDir/web
-			        
-			# Possible values include: debug, info, notice, warn, error, crit,
-        	# alert, emerg.
-        	LogLevel warn
-
-        	CustomLog $rootDir/logs/access.log combined
-			ErrorLog $rootDir/logs/error.log
-			
-			<Directory $rootDir/web>
-				Options +FollowSymLinks
-				AllowOverride all
-			</Directory>
-			
-			# suexec enabled
-			<IfModule mod_suexec.c>
-				SuexecUserGroup $owner $owner
-			</IfModule>
-			
-			# add support for apache mpm_itk
-			<IfModule mpm_itk_module>
-				AssignUserId $owner $owner
-			</IfModule>
-
-		</VirtualHost>
-		<VirtualHost *:443>
-			ServerAdmin $email
-			ServerName $domain
-			ServerAlias *.$domain
-			DocumentRoot $rootDir/web
-			        
-			# Possible values include: debug, info, notice, warn, error, crit,
-        	# alert, emerg.
-        	LogLevel warn
-
-        	CustomLog $rootDir/logs/access.log combined
-			ErrorLog $rootDir/logs/error.log
-			
-			<Directory $rootDir/web>
-				Options +FollowSymLinks
-				AllowOverride all
-			</Directory>
-			
-			# suexec enabled
-			<IfModule mod_suexec.c>
-				SuexecUserGroup $owner $owner
-			</IfModule>
-			
-			# add support for apache mpm_itk
-			<IfModule mpm_itk_module>
-				AssignUserId $owner $owner
-			</IfModule>
-			
-			<IfModule mod_ssl.c>
-				SSLEngine on
-				SSLProtocol All -SSLv2 -SSLv3
-				SSLCertificateFile $rootDir/ssl/$domain.crt
-				SSLCertificateKeyFile $rootDir/ssl/$domain.key
-			</IfModule>
-
-		</VirtualHost>" > $sitesAvailabledomain
+		fi	
+		
+		### Forcing permission
+		echo "umask 027" > $rootDir/.bashrc
+		chown $owner:$owner $rootDir/.bashrc
+		
+		### create subdirectory (data and logs
+		mkdir $rootDir/web
+		mkdir $rootDir/logs
+		mkdir $rootDir/ssl
+				
+		### change user
+		chown root:$owner $rootDir
+		chown root:$owner $rootDir/web
+		chown root:$owner $rootDir/logs
+		chown root:$owner $rootDir/ssl
+		
+		### give permission to subdirectory (A+RX)
+		chmod 1750 $rootDir
+		chmod 1750 $rootDir/web
+		chmod 1750 $rootDir/logs
+		chmod 1750 $rootDir/ssl
+		
+		### write test file in the new domain dir
+		if ! echo "<?php echo phpversion(); ?>" > $rootDir/web/phpversion.php
 		then
-			echo -e $"There is an ERROR creating $domain file"
-			exit;
+			echo $"ERROR: Not able to write in file $rootDir/phpversion.php. Please check permissions"
+			exit 3;
 		else
-			echo -e $"\nNew Virtual Host Created\n"
+			echo $"Added content to $rootDir/phpversion.php"
 		fi
+		
+		chown $owner:$owner $rootDir/web/phpversion.php
 
-		### enable website
-		a2ensite $siteName
-
-		### restart Apache
-		systemctl reload apache2
+		setVirtualHostConfFile $sitesAvailabledomain
 
 		### show the finished message
 		echo -e $"Complete! \nYou now have a new Virtual Host \nYour new host is: http://$domain \nAnd its located at $rootDir"
+		echo -e $"Don't forget to enable it"
 		
-	elif [ "$action" == 'disable' ]
+	elif [ "$action" == 'enable' ]; then
 		### check whether domain already exists
 		if ! [ -e $sitesAvailabledomain ]; then
 			echo -e $"This domain does not exist.\nPlease try another one"
-			exit;
+			exit 2;
+		fi
+		
+		### disable website
+		a2ensite $siteName
+		
+		### reload Apache
+		systemctl reload apache2
+		
+		### show the finished message
+		echo -e $"Complete! \n$siteName has been enabled"	
+		
+	elif [ "$action" == 'disable' ]; then
+		### check whether domain already exists
+		if ! [ -e $sitesAvailabledomain ]; then
+			echo -e $"This domain does not exist.\nPlease try another one"
+			exit 2;
 		fi
 		
 		### disable website
@@ -215,12 +235,21 @@ if [ "$action" == 'create' ]
 		### show the finished message
 		echo -e $"Complete! \n$siteName has been disabled"
 		
-	elif [ "$action" == 'delete' ]
+	elif [ "$action" == 'delete' ]; then
 		### check whether domain already exists
 		if ! [ -e $sitesAvailabledomain ]; then
 			echo -e $"This domain does not exist.\nPlease try another one"
-			exit;
+			exit 2;
 		fi
+		
+		while true; do
+		    read -p "Do you wish to commit this configuration?" yn
+    		case $yn in
+        		[Yy]* ) break;break;;
+        		[Nn]* ) exit;;
+        		* ) echo "Please answer yes or no.";;
+    		esac
+		done
 		
 		### disable website
 		a2dissite $siteName
@@ -236,8 +265,10 @@ if [ "$action" == 'create' ]
 		
 			sysuser=$(ls -ld $rootDir | awk '{print $3}')
 			
-			deluser --system --group --remove-home $sysuser
 			deluser --system --remove-home $sysuser
+			deluser --system --group --remove-home $sysuser
+			
+			rml -rf $rootDir
 		
 			echo -e $"Directory deleted"
 		else
@@ -247,9 +278,20 @@ if [ "$action" == 'create' ]
 		### show the finished message
 		echo -e $"Complete!\nYou just removed Virtual Host $siteName"
 		exit 0;
+	elif [ "$action" == 'conf' ]; then
+		### check whether domain already exists
+		if ! [ -e $sitesAvailabledomain ]; then
+			echo -e $"This domain does not exist.\nPlease try another one"
+			exit 2;
+		fi
 		
+		setVirtualHostConfFile $sitesAvailabledomain
+		
+		### show the finished message
+		echo -e $"Complete!\nYou just updated Virtual Host $siteName"
+		exit 0;
 	else
 		### show the finished message
-		echo -e $"UNKNOWN"
-		exit 0;
+		echo $"You need to prompt for action (create, enable, disable, conf or delete) -- Lower-case only"
+		exit 1;
 fi
